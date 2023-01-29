03fedba6772a640c8dd22381040f99de4a9e4c21
priyar1 <122872326+priyar1@users.noreply.github.com>
2023-01-29 23:54:47 +0530

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,59 @@
+# print(type(0))
+# print(type(4))
+# print(type(-1))
+# print(type(-4523454))
+
+# print(type(0.2))
+# print(type(.9))
+# print(type(-3.4))
+# print(type(-123234.2324))
+
+# print(type('hi'))
+# print(type("hi"))
+# print(type('''hi'''))
+# print(type("""hi"""))
+
+
+# print(type('py'))
+# print(type('7'))
+# print(type('9.4'))
+
+# a=6
+# print(a,type(a))
+# print(int(a))
+# print(float(a))
+# print(str(a))
+
+# p='Python' ; v=3.1
+# print(type(p))
+# print(type(v))
+# print(p+v)
+
+# a=4 ; b=6
+# print(a+b)
+# a=4.8 ; b=6
+# print(a+b)
+# a=4.8 ; b=6.3
+# print(a+b)
+# a=4 ; b='6.3'
+# print(str(a)+b)
+
+# print('hi'+'hey')
+# print('4'+'8')
+
+# b=7.4
+# print(int(b))
+# print(str(b))
+
+# c='hi'
+# print(int(c))
+# print(float(c))
+
+# d='9'
+# print(int(d))
+# print(float(d))
+
+# e='5.8'
+# print(float(e))
+# h=float(e)
+# print(int(h))
